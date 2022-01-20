227e5088a97f91057bd41f56d101595ecb51c4e3
sofikin <sofia.kalpin@khk.ee>
2022-01-20 13:57:16 +0200

    As a user I can add meals and their calories [Delivers #180976838]

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const ItemCtrl = (function (){
         this.calories = calories
     }
 
-    // Data Structure
+    // Data Structures
     const data = {
         items: [
             //{id: 0, name: "Steak Dinner", calories: 1200},
